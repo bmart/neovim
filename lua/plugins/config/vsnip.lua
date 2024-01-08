200b5739d8a96d57af491b1957ea990ee0542054
brian.martin <brian.martin@rcgtconsulting.com>
2024-01-08 08:34:16 -0500

    fiddle with vsnip bindings

--- lua/plugins/config/vsnip.lua
+++ lua/plugins/config/vsnip.lua
@@ -1,7 +1,7 @@
 -- VSnip
-vim.g.vsnip_snippet_dir = '~/.config/nvim/snippets'
+vim.g.vsnip_snippet_dir = vim.fn.stdpath("config") .. "/snippets"
 -- imap <expr> <C-j>   vsnip#available(1)  ? '<Plug>(vsnip-expand)'         : '<C-j>'
 -- imap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
 -- smap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
 
-vim.keymap.set('n','<expr> <C-l>',"vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'")
+vim.keymap.set('i','<C-l>','<Plug>(vsnip-expand-or-jump)', { silent = true})
