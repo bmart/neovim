feab55efc5008ffb0045477c45087e2e331ccbce
brian.martin <brian.martin@rcgtconsulting.com>
2024-02-05 07:45:24 -0500

    added olama

--- lua/plugins/manager.lua
+++ lua/plugins/manager.lua
@@ -58,6 +58,39 @@ local plugins = {
       vim.g.mkdp_filetypes = { "markdown" }
     end,
     ft = { "markdown" },
+  },
+  {
+    "nomnivore/ollama.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+
+    -- All the user commands added by the plugin
+    cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+    
+    keys = {
+      -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+      {
+        "<leader>oo",
+        ":<c-u>lua require('ollama').prompt()<cr>",
+        desc = "ollama prompt",
+        mode = { "n", "v" },
+      },
+
+      -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+      {
+        "<leader>oG",
+        ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+        desc = "ollama Generate Code",
+        mode = { "n", "v" },
+      },
+    },
+    ---@type Ollama.Config
+    opts = {
+      -- your configuration overrides
+      model = "mixtral:8x7b",
+      url = "https://chadgpt.oxaro.io:8443/v1/engines",
+    }
   }
 }
 
